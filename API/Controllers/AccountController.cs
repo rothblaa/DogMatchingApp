e0abc0c96ceeebf14262463c5382b695d184b9bd
rothblaa <adam.j.rothblatt@outlook.com>
2024-10-13 17:33:06 -0500

    started work on register account dto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,22 +2,25 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")] //account/register
-    public async Task<ActionResult<AppUser>> Register(string userName, string password)
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken!");
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
         {
-            UserName = userName,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -25,5 +28,10 @@ public class AccountController(DataContext context) : BaseApiController
         await context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string username)
+    {
+        return await context.Users.AnyAsync(x => x.UserName.ToLower() == username.ToLower()); //bob != Bob
     }
 }
